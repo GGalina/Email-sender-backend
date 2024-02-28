bf311b555a27933d52aad04d9ffb66fc10557add
GGalina <71042425+GGalina@users.noreply.github.com>
2024-02-28 23:30:40 +0400

    UPDATE: mailModel fixed typo

--- mailModel.js
+++ mailModel.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
 
 const emailSchema = Joi.object({
-    teacher: Joi.string.min(2).required(),
+    teacher: Joi.string().min(2).required(),
     name: Joi.string().min(2).required(),
     email: Joi.string().email().pattern(emailPattern).required(),
     phone: Joi.string().min(6).required(),
